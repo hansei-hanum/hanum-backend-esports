675890b145de1e86eedaaf71e8f1d8d511b0edcc
leeyeonwoo <rexrex1105@gmail.com>
2024-11-06 22:27:22 +0900

    refactor: edit db init script

--- dbInit.py
+++ dbInit.py
@@ -1,4 +1,8 @@
 from database.core import Base, engine
-import sqlalchemy
+import asyncio
 
-Base.metadata.create_all(engine)
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(init_db())
